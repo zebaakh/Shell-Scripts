e6784bb5d398cc8962fdeffa8bbcf7d92b2666c4
zeba akhtari <zeba.akhtari@abzooba.com>
2024-01-18 00:27:23 +0530

    push all files

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,8 +3,8 @@
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 IMAGE_ID=ami-0d4949a5a5686445c
 SECURITY_GROUP_ID=sg-0d09274b7b3d19419
-#DOMAIN_NAME=joindevops.online
-#HOSTED_ZONE_ID=Z0308214GYCUYHGJHT8R
+DOMAIN_NAME=joindevops.online
+HOSTED_ZONE_ID=Z06112601XX8DKA9TU7T7
 
 # Improvement: Use a function to create an EC2 instance
 create_ec2_instance() {
@@ -26,8 +26,28 @@ create_ec2_instance() {
                         | jq -r '.Instances[0].PrivateIpAddress')
     
     echo "Created $SERVICE_NAME instance: $IP_ADDRESS"
+    update_route53_record $SERVICE_NAME $IP_ADDRESS
 }
+# Function to update Route 53 record
+update_route53_record() {
+    local SERVICE_NAME=$1
+    local IP_ADDRESS=$2
 
+    echo "Updating Route 53 record for $SERVICE_NAME"
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
+    {
+        "Changes": [{
+            "Action": "CREATE",
+            "ResourceRecordSet": {
+                "Name": "'$SERVICE_NAME.$DOMAIN_NAME'",
+                "Type": "A",
+                "TTL": 300,
+                "ResourceRecords": [{"Value": "'$IP_ADDRESS'"}]
+            }
+        }]
+    }
+    '
+}
 # Loop through service names and create EC2 instances
 for SERVICE_NAME in "${NAMES[@]}"; do
     create_ec2_instance $SERVICE_NAME
